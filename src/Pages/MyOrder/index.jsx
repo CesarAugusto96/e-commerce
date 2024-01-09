2e4e730123a9bebe22d08175e40a95620670ec53
Cesar Caballero <cesarcaballero203@gmail.com>
2024-01-09 15:39:39 -0600

    Pagina de Checkout agregada

--- src/Pages/MyOrder/index.jsx
+++ src/Pages/MyOrder/index.jsx
@@ -1,9 +1,29 @@
+import { useContext } from 'react';
+import { ShoppingCartContext } from '../../Context';
+import OrderCard from '../../Components/OrderCard';
 import React from 'react';
 import Layout from '../../Layout';
 
 const MyOrder = () => {
+  const context = useContext(ShoppingCartContext) 
+
   return (
-    <Layout>MyOrder</Layout>
+    <Layout>
+      MyOrder
+      <div className='flex flex-col w-80'>
+            {
+                context.order?.slice(-1)[0].products.map(product => (
+                    <OrderCard 
+                        key={product.id}
+                        id={product.id}
+                        title={product.title}
+                        imageURL={product.image}
+                        price={product.price}
+                        />
+                ))
+            }
+            </div>
+    </Layout>
   )
 }
 
